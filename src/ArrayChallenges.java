4bde684e025a21cd0c005e2b54ba4ca074860c94
jonathan.egan <jonathan.egan@MU-C1-12.mullen.pvt.k12.co.us>
2019-09-13 11:55:14 -0600

    commit

--- src/ArrayChallenges.java
+++ src/ArrayChallenges.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class ArrayChallenges 
 {
@@ -18,7 +17,6 @@ public class ArrayChallenges
 		
 		String[] myArray = new String[numberOfSweets]; 
 		
-		
 		for(int i = 0; i < numberOfSweets; i++)
 		{
 			System.out.println("Enter the name of the sweet " +(i+1));
@@ -36,21 +34,50 @@ public class ArrayChallenges
 	private static void inputEight() 
 	{
 		Scanner intInput = new Scanner(System.in);
-		System.out.println("Input eight numbers ");
+		System.out.println("Input the number 8. You will be asked to enter some numbers.");
 		int numbers = intInput.nextInt();
 		
 		int[] myArray = new int[numbers];
 		
 		for(int i = 0; i < numbers; i++)
 		{
+			System.out.println("Enter number " + (i+1));
 			myArray[i] = intInput.nextInt();
 		}
 		intInput.close();
+	
+		int even = 0; 
+	    int odd = 0;
+	    
+	    for (int i = 0; i < numbers; i++) 
+	    { 
+	        if (i % 2 == 0)
+	        {
+	        	System.out.println(even += myArray[i]);
+	        }
+	        else
+	        {
+	        	System.out.println(odd += myArray[i]); 
+	        }
+	    } 
 	}
 
 	private static void randomTwoDigit() 
 	{
+		System.out.println("Generating five random two-digit numbers and printing them out.");
 		
+		int[] myArray = new int[5];
 		
+		for(int i = 0; i < myArray.length; i++)
+		{
+			 myArray[i] = (int)(Math.random()*20 + 1);
+			 System.out.println(myArray[i]);
+		}
+		
+		System.out.println("Here are the numbers backwards.");
+		for (int i = myArray.length-1; i >= 0; i--) 
+		{  
+            System.out.println(myArray[i]);  
+		}
 	}
 }
